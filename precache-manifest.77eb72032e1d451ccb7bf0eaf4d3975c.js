68919bbae5f2eba0c36bb1518b9ba55a167c597e
angsalacata <angsala@umich.edu>
2023-08-11 17:51:16 -0500

    Completed content in Experience tab

---
+++ precache-manifest.77eb72032e1d451ccb7bf0eaf4d3975c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "cda7002ffd0246b9995d",
+    "url": "/static/css/main.11dbdae3.chunk.css"
+  },
+  {
+    "revision": "cda7002ffd0246b9995d",
+    "url": "/static/js/main.cda7002f.chunk.js"
+  },
+  {
+    "revision": "575592be2d3e253bb90c",
+    "url": "/static/js/1.575592be.chunk.js"
+  },
+  {
+    "revision": "6da68919c53c23ca6078",
+    "url": "/static/js/2.6da68919.chunk.js"
+  },
+  {
+    "revision": "5d2309f0e0fbd97d0b20",
+    "url": "/static/js/runtime~main.5d2309f0.js"
+  },
+  {
+    "revision": "6b2fe4f17fcf6811dde548766cf566e1",
+    "url": "/static/media/head-shot-picture copy cropped.6b2fe4f1.png"
+  },
+  {
+    "revision": "4613e1ee7b493178df70ce52f3a60c09",
+    "url": "/static/media/contact-photo-copy.4613e1ee.png"
+  },
+  {
+    "revision": "b8641c8080b372a61348136899a3184f",
+    "url": "/static/media/down-arrow-svgrepo-com.b8641c80.svg"
+  },
+  {
+    "revision": "1b6fe5086a3977b8406c65b567183472",
+    "url": "/index.html"
+  }
+];
